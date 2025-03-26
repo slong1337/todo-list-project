47eacd1d809b1aa05ba760a3cd149c82e45dab2d
slong1337 <kuznecov.pasha@inbox.ru>
2025-03-26 18:42:38 +0300

    add base_path

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const BASE_PATH = process.env.BASE_PATH
+
 export default defineConfig({
   plugins: [
     remix({
       appDirectory: 'src',
+      ssr: false,
+      basename: BASE_PATH ? ${BASE_PATH}/ : '/',
+      publicPath: BASE_PATH ? ${BASE_PATH}/ : '/',
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
